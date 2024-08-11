0f78c9a95e6e0f3124191b03af668e59eb16462b
alpacaive <w6352800@naver.com>
2024-08-11 11:17:06 +0900

    엔티티 클래스 개발2

--- src/main/java/jpabook/springboot_jpa/domain/Address.java
+++ src/main/java/jpabook/springboot_jpa/domain/Address.java
@@ -11,4 +11,16 @@ public class  Address {
     private String street;
     private String zipcode;
 
+    // jpa 스펙상 만들어 놓은 것 = 사용 X
+    // JPA가 이런 제약을 두는 이유는 JPA 구현 라이브러리가 객체를 생성할 대 리플랙션 같은 기술을 사용할 수 있도록 지원해야 하기 때문
+    protected Address() {
+
+    }
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
+
 }
