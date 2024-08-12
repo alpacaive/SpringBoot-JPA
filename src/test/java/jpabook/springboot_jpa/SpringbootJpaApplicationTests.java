b3205ee27e52592f846137b3b655f62a75e89687
alpacaive <w6352800@naver.com>
2024-08-12 09:13:31 +0900

    회원 기능 테스트

--- src/test/java/jpabook/springboot_jpa/SpringbootJpaApplicationTests.java
+++ src/test/java/jpabook/springboot_jpa/SpringbootJpaApplicationTests.java
@@ -3,11 +3,19 @@ package jpabook.springboot_jpa;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class SpringbootJpaApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void 이름 () throws Exception {
+	    // given
+
+	    // when
+
+	    // then
+
 	}
 
 }
