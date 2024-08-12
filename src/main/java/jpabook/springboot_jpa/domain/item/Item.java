c5a4c9fc5b35d7a4d4b9265239886c525c5e7449
alpacaive <w6352800@naver.com>
2024-08-13 04:52:57 +0900

    상품 엔티티 개발 (비즈니스 로직 추가)

--- src/main/java/jpabook/springboot_jpa/domain/item/Item.java
+++ src/main/java/jpabook/springboot_jpa/domain/item/Item.java
@@ -2,6 +2,7 @@ package jpabook.springboot_jpa.domain.item;
 
 import jakarta.persistence.*;
 import jpabook.springboot_jpa.domain.Category;
+import jpabook.springboot_jpa.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -26,5 +27,25 @@ public class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    // === 비즈니스 로직 === //
+
+    /**
+     * stock 증가
+     */
+    public void addStockQuantity(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * stock 감소
+     */
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 
 }
