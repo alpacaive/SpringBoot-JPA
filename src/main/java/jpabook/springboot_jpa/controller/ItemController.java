a1634d42f9dd3903958868cd2269ec8632fc5518
alpacaive <w6352800@naver.com>
2024-08-22 17:20:07 +0900

    상품등록

---
+++ src/main/java/jpabook/springboot_jpa/controller/ItemController.java
@@ -0,0 +1,39 @@
+package jpabook.springboot_jpa.controller;
+
+import jpabook.springboot_jpa.domain.item.Book;
+import jpabook.springboot_jpa.service.ItemService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class ItemController {
+
+    private final ItemService itemService;
+
+    @GetMapping("/items/new")
+    public String createForm(Model model) {
+        model.addAttribute("form", new BookForm());
+        return "items/createItemForm";
+    }
+
+    @PostMapping("/items/new")
+    public String create(BookForm form) {
+        Book book = new Book();
+
+        // static 생성자 메서드 만들어서 의도에 맞게 생성자를 사용하는 것이 가장 좋은 설계 -> setter 도 날릴 수 있음
+        book.setName(form.getName());
+        book.setPrice(form.getPrice());
+        book.setStockQuantity(form.getStockQuantity());
+        book.setAuthor(form.getAuthor());
+        book.setIsbn(form.getIsbn());
+
+        itemService.saveItem(book);
+        return "redirect:/";
+
+    }
+
+}
