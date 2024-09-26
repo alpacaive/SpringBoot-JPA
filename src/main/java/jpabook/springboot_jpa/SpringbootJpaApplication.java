29793ee8bd1ce2ad1c4504f7f873139c69ab6102
alpacaive <w6352800@naver.com>
2024-09-27 08:43:19 +0900

    간단한 주문 조회 V1 : 엔티티를 직접 노출

--- src/main/java/jpabook/springboot_jpa/SpringbootJpaApplication.java
+++ src/main/java/jpabook/springboot_jpa/SpringbootJpaApplication.java
@@ -1,13 +1,31 @@
 package jpabook.springboot_jpa;
 
+import com.fasterxml.jackson.datatype.hibernate5.jakarta.Hibernate5JakartaModule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringbootJpaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootJpaApplication.class, args);
+	}
+
+	/**
+	 * 이 옵션을 키면 `order -> member` , `member -> orders` 양방향 연관관계를 계속 로딩하게 된다.
+	 * 따라서 @JsonIgnore 옵션을 한곳에 주어야 한다
+	 * 주의: 엔티티를 직접 노출할 때는 양방향 연관관계가 걸린 곳은 꼭! 한곳을 `@JsonIgnore` 처리 해야 한다. 안그러면 양쪽을 서로 호출하면서 무한 루프가 걸린다.
+	 * cf) 엔티티를 API 응답으로 외부로 노출하는 것은 좋지 않다. 따라서 `Hibernate5Module` 를 사용하기 보다는 DTO로 변환해서 반환하는 것이 더 좋은 방법이다.
+	 * --- 주의 ---
+	 * 지연 로딩(LAZY)을 피하기 위해 즉시 로딩(EARGR)으로 설정하면 안된다!
+	 * 즉시 로딩 때문에 연관관계가 필요 없는 경우에도 데이터를 항상 조회해서 성능 문제가 발생할 수 있다.
+	 * 즉시 로딩으로 설정하면 성능 튜닝이 매우 어려워 진다.
+	 * 항상 지연 로딩을 기본으로 하고, 성능 최적화가 필요한 경우에는 페치 조인(fetch join)을 사용해라!(V3에서 설명)
+	 */
+	@Bean
+	Hibernate5JakartaModule hibernate5Module() {
+		return new Hibernate5JakartaModule();
 	}
 
 }
---
+++ src/main/java/jpabook/springboot_jpa/api/OrderSimpleApiController.java
@@ -0,0 +1,34 @@
+package jpabook.springboot_jpa.api;
+
+import jpabook.springboot_jpa.domain.Order;
+import jpabook.springboot_jpa.repository.OrderRepository;
+import jpabook.springboot_jpa.repository.OrderSearch;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * xToOne(ManyToOne, OneToOne)
+ * Order
+ * Order -> Member
+ * Order -> Delivery
+ */
+@RestController
+@RequiredArgsConstructor
+public class OrderSimpleApiController {
+
+    private final OrderRepository orderRepository;
+
+    @GetMapping("/api/v1/simple-orders")
+    public List<Order> orderV1() {
+        List<Order> all = orderRepository.findAllByString(new OrderSearch());
+        for (Order order : all) {
+            order.getMember().getName(); // Lazy 강제 초기화
+            order.getDelivery().getAddress(); // Lazy 강제 초기화
+        }
+        return all;
+    }
+
+}
