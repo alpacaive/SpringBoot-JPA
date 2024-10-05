ae46e2b6d7bae00ef9d0ca1a40cdd0de1af35172
alpacaive <w6352800@naver.com>
2024-10-05 15:38:41 +0900

    주문 조회 V2 : 엔티티를 DTO로 변환

--- src/main/java/jpabook/springboot_jpa/api/OrderApiController.java
+++ src/main/java/jpabook/springboot_jpa/api/OrderApiController.java
@@ -1,14 +1,20 @@
 package jpabook.springboot_jpa.api;
 
+import jpabook.springboot_jpa.domain.Address;
 import jpabook.springboot_jpa.domain.Order;
 import jpabook.springboot_jpa.domain.OrderItem;
+import jpabook.springboot_jpa.domain.OrderStatus;
 import jpabook.springboot_jpa.repository.OrderRepository;
 import jpabook.springboot_jpa.repository.OrderSearch;
+import lombok.Data;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * V1 엔티티가 직접 노출
@@ -60,6 +66,53 @@ public class OrderApiController {
         return all;
     }
 
+    @GetMapping("/api/v2/orders")
+    public List<OrderDto> ordersV2() {
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+        List<OrderDto> result = orders.stream()
+                .map(o -> new OrderDto(o))
+                .collect(Collectors.toList());
+        return result;
+    }
 
+
+    /**
+     * V2 용 DTO
+     */
+    @Getter
+    static class OrderDto {
+
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+        private List<OrderItemDto> orderItems;
+
+        public OrderDto(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName();
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress();
+            orderItems = order.getOrderItems().stream()
+                    .map(orderItem -> new OrderItemDto(orderItem))
+                    .collect(Collectors.toList());
+        }
+
+    }
+
+    @Getter
+    static class OrderItemDto {
+        private String itemName; // 상품명
+        private int orderPrice; // 주문 가격
+        private int count; // 주문 수량
+
+        public OrderItemDto(OrderItem orderItem) {
+            itemName = orderItem.getItem().getName();
+            orderPrice = orderItem.getOrderPrice();
+            count = orderItem.getCount();
+        }
+    }
 
 }
--- src/main/java/jpabook/springboot_jpa/domain/OrderItem.java
+++ src/main/java/jpabook/springboot_jpa/domain/OrderItem.java
@@ -1,5 +1,6 @@
 package jpabook.springboot_jpa.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import jpabook.springboot_jpa.domain.item.Item;
 import lombok.AccessLevel;
@@ -22,6 +23,7 @@ public class OrderItem {
     @JoinColumn(name = "item_id")
     private Item item;
 
+    @JsonIgnore
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "order_id")
     private Order order;
