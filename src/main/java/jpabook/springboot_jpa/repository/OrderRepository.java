b15b910b5e80710066646e1ff42d839594e8c608
alpacaive <w6352800@naver.com>
2024-10-05 17:00:52 +0900

    주문 조회 V3 : 엔티티를 DTO로 변환 - 페치 조인 최적화

--- src/main/java/jpabook/springboot_jpa/repository/OrderRepository.java
+++ src/main/java/jpabook/springboot_jpa/repository/OrderRepository.java
@@ -110,6 +110,27 @@ public class OrderRepository {
     }
 
     /**
+     * - 패치 조인으로 SQL이 1번만 실행됨
+     * - distinct 를 사용한 이유는 1대다 조인이 있으므로 데이터베이스 row가 증가한다.
+     *   그 결과 같은 order 엔티티의 조회 수도 증가하게 된다
+     *   JPA 의 distinct 는 SQL에 distinct를 추가하고, 더해서 같은 엔티티가 조회되면, 애플리케이션에서 중복을 걸러준다
+     *   이 예에서 order 가 페치 조인 때문에 중복 조회 되는 것을 막아준다
+     *  ( Hibernate 6.0부터는 distinct가 자동 적용 )
+     *
+     * 단점
+     * - 페이징 불가능
+     */
+    public List<Order> findAllWithItem() {
+        return em.createQuery(
+                "select o from Order o" +
+                        " join fetch o.member m" +
+                        " join fetch o.delivery d" +
+                        " join fetch o.orderItems oi" +
+                        " join fetch oi.item i", Order.class)
+                .getResultList();
+    }
+
+    /**
      * Querydsl 로 처리
      */
 //    public List<Order> findAll(OrderSearch orderSearch) {
