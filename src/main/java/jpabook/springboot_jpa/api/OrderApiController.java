7697f21101eca2e4b6fb83283cdc0864bd37acfd
alpacaive <w6352800@naver.com>
2024-10-11 20:40:51 +0900

    주문 조회 V3.1 : 엔티티를 DTO로 변환 - 페이징과 한계 돌파

--- src/main/java/jpabook/springboot_jpa/api/OrderApiController.java
+++ src/main/java/jpabook/springboot_jpa/api/OrderApiController.java
@@ -10,6 +10,7 @@ import lombok.Data;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
@@ -82,6 +83,25 @@ public class OrderApiController {
         for (Order order : orders) {
             System.out.println("order ref = " + order + " id = " + order.getId());
         }
+
+        List<OrderDto> result = orders.stream()
+                .map(o -> new OrderDto(o))
+                .collect(Collectors.toList());
+        return result;
+    }
+
+    /**
+     * V3.1 엔티티를 조회해서 DTO로 변환 고려
+     * - ToOne 관계만 우선 모두 페치 조인으로 최적화
+     * - 컬렉션 관계는 hibernate.default_batch_fetch_size, @BatchSize 로 최적화
+     */
+    @GetMapping("/api/v3.1/orders")
+    public List<OrderDto> ordersV3_page(
+            @RequestParam(value = "offset", defaultValue = "0") int offset,
+            @RequestParam(value = "limit", defaultValue = "100") int limit)
+    {
+        List<Order> orders = orderRepository.findAllWithMemberDelivery(offset, limit);
+        // Order 를 기준으로ToOne 관계 걸린 애는 다 패치조인으로 한방에 가져옴
 
         List<OrderDto> result = orders.stream()
                 .map(o -> new OrderDto(o))
--- src/main/java/jpabook/springboot_jpa/repository/OrderRepository.java
+++ src/main/java/jpabook/springboot_jpa/repository/OrderRepository.java
@@ -101,6 +101,25 @@ public class OrderRepository {
         return query.getResultList();
     }
 
+    /**
+     * Querydsl 로 처리
+     */
+//    public List<Order> findAll(OrderSearch orderSearch) {
+//
+//        QOrder order = QOrder.order;
+//        QMember member = QMember.member;
+//
+//        return query
+//                .select(order)
+//                .from(order)
+//                .join(order.member, member)
+//                .where(statusEq(orderSearch.getOrderStatus()),
+//                        nameLike(orderSearch.getMemberName()))
+//                .limit(1000)
+//                .fetch();
+//    }
+
+
     public List<Order> findAllWithMemberDelivery() {
         return em.createQuery(
                         "select o from Order o" +
@@ -131,21 +150,23 @@ public class OrderRepository {
     }
 
     /**
-     * Querydsl 로 처리
+     * 한계돌파
+     * 페이징 + 컬렉션 엔티티를 함께 조회 문제는 이 방법으로 대부분 해결 가능
+     * - 먼저 ToOne 관계를 모두 페치조인 한다.
+     *   ToOne 관계는 row 수를 증가시키지 않으므로 페이징 쿼리에 영향을 주지 않는다
+     * - 컬렉션은 지연 로딩으로 조회한다
+     * - 지연 로딩은 성능 최적화를 위해 hibernate.default_batch_fetch_size, @BatchSize 를 적용한다
+     *    - hibernate.default_batch_fetch_size : 글로벌 성정
+     *    - @BatchSize : 개별 최적화
+     *    - 이 옵션을 사용하면 컬렉션이나 프록시 객체를 한꺼번에 설정한 size 만큼 IN 쿼리로 조회한다
      */
-//    public List<Order> findAll(OrderSearch orderSearch) {
-//
-//        QOrder order = QOrder.order;
-//        QMember member = QMember.member;
-//
-//        return query
-//                .select(order)
-//                .from(order)
-//                .join(order.member, member)
-//                .where(statusEq(orderSearch.getOrderStatus()),
-//                        nameLike(orderSearch.getMemberName()))
-//                .limit(1000)
-//                .fetch();
-//    }
-
+    public List<Order> findAllWithMemberDelivery(int offset, int limit) {
+        return em.createQuery(
+                "select o from Order o" +
+                        " join fetch o.member m" +
+                        " join fetch o.delivery d", Order.class)
+                .setFirstResult(offset)
+                .setMaxResults(limit)
+                .getResultList();
+    }
 }
