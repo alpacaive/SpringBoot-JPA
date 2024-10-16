31bbded4347d944d4870cc9a73e5f2c10a223ccd
alpacaive <w6352800@naver.com>
2024-10-16 17:00:09 +0900

    주문 조회 V5 : JPA에서 DTO 직접 조회 - 컬렉션 조회 최적화

--- src/main/java/jpabook/springboot_jpa/api/OrderApiController.java
+++ src/main/java/jpabook/springboot_jpa/api/OrderApiController.java
@@ -125,6 +125,11 @@ public class OrderApiController {
         return orderQueryRepository.findOrderQueryDtos();
     }
 
+    @GetMapping("/api/v5/orders")
+    public List<OrderQueryDto> ordersV5() {
+        return orderQueryRepository.findAllByDto_optimization();
+    }
+
 
     /**
      * V2 용 DTO
