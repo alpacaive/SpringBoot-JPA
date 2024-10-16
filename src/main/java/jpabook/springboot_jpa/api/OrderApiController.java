e2964711fd56edd15978856d501bac8e0e6b0dfd
alpacaive <w6352800@naver.com>
2024-10-16 17:44:04 +0900

    주문 조회 V6 : JPA에서 DTO 직접 조회 - 플랫 데이터 최적화

--- src/main/java/jpabook/springboot_jpa/api/OrderApiController.java
+++ src/main/java/jpabook/springboot_jpa/api/OrderApiController.java
@@ -6,6 +6,8 @@ import jpabook.springboot_jpa.domain.OrderItem;
 import jpabook.springboot_jpa.domain.OrderStatus;
 import jpabook.springboot_jpa.repository.OrderRepository;
 import jpabook.springboot_jpa.repository.OrderSearch;
+import jpabook.springboot_jpa.repository.order.query.OrderFlatDto;
+import jpabook.springboot_jpa.repository.order.query.OrderItemQueryDto;
 import jpabook.springboot_jpa.repository.order.query.OrderQueryDto;
 import jpabook.springboot_jpa.repository.order.query.OrderQueryRepository;
 import lombok.Data;
@@ -19,25 +21,27 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static java.util.stream.Collectors.*;
+
 /**
  * V1 엔티티가 직접 노출
  * - 엔티티가 변하면 API 스펙이 변한다
  * - 트랜잭션 안에서 지연 로딩 필요
  * - 양방향 연관관계 문제
- *
+ * <p>
  * V2 엔티티를 조회해서 DTO로 변환 (fetch join 사용X)
  * - 트랜잭션 안에서 지연 로딩 필요
- *
+ * <p>
  * V3 엔티티를 조회해서 DTO로 변환 (fetch join 사용O)
  * - 페이징 시에는 N 부분을 포기해야 함
  * - 대신 batch fetch size? 옵션 주면 N -> 1 쿼리로 변경 가능
- *
+ * <p>
  * V4 JPA 에서 DTO 로 바로 조회, 컬렉션 N 조회 (1 + N Query)
  * - 페이징 가능
- *
+ * <p>
  * V5 JPA 에서 DTO 로 바로 조회 최적화 버전 (1 + 1 Query)
  * - 페이징 가능
- *
+ * <p>
  * V6 JPA 에서 DTO로 바로 조회, 플랫 데이터 (1 Query) (1 Query)
  * - 페이징 불가능
  */
@@ -52,7 +56,7 @@ public class OrderApiController {
      * V1 엔티티 직접 노출
      * - Hibernate5Module 모듈 등록, Lazy = null 처리
      * - 양방향 관계 문제 발생 -> @JsonIgnore
-     *
+     * <p>
      * - orderItem, item 관계를 직접 초기화하면 Hibernate5Module 설정에 의해 엔티티를 JSON으로 생성한다
      * - 양방향 연관관계면 무한 루프에 걸리지 않게 한곳에 @JsonIgnore 를 추가해야 한다
      * - 엔티티를 직접 노출하므로 좋은 방법은 아니다
@@ -75,7 +79,7 @@ public class OrderApiController {
         List<Order> orders = orderRepository.findAllByString(new OrderSearch());
         List<OrderDto> result = orders.stream()
                 .map(o -> new OrderDto(o))
-                .collect(Collectors.toList());
+                .collect(toList());
         return result;
     }
 
@@ -89,7 +93,7 @@ public class OrderApiController {
 
         List<OrderDto> result = orders.stream()
                 .map(o -> new OrderDto(o))
-                .collect(Collectors.toList());
+                .collect(toList());
         return result;
     }
 
@@ -101,22 +105,21 @@ public class OrderApiController {
     @GetMapping("/api/v3.1/orders")
     public List<OrderDto> ordersV3_page(
             @RequestParam(value = "offset", defaultValue = "0") int offset,
-            @RequestParam(value = "limit", defaultValue = "100") int limit)
-    {
+            @RequestParam(value = "limit", defaultValue = "100") int limit) {
         List<Order> orders = orderRepository.findAllWithMemberDelivery(offset, limit);
         // Order 를 기준으로ToOne 관계 걸린 애는 다 패치조인으로 한방에 가져옴
 
         List<OrderDto> result = orders.stream()
                 .map(o -> new OrderDto(o))
-                .collect(Collectors.toList());
+                .collect(toList());
         return result;
     }
 
     /**
      * Query : 루트 1번, 컬렉션 N번 실행
      * ToOne(N:1, 1:1) 관계들을 먼저 조회하고, ToMany(1 : N) 관계는 각각 별도로 처리한다
-     *    - ToOne 관계는 조인해도 데이터 row 수가 증가하지 않는다
-     *    - ToMany(1:N) 관계는 조인하면 row 수가 증가한다
+     * - ToOne 관계는 조인해도 데이터 row 수가 증가하지 않는다
+     * - ToMany(1:N) 관계는 조인하면 row 수가 증가한다
      * row 수가 증가하지 않는 ToOne 관계는 조인으로 최적화 하기 쉬우므로 한번에 조회하고
      * , ToMany 관계는 최적화하기 어려우므로 findOrderItems() 같은 별도의 메서드로 조회한다
      */
@@ -125,9 +128,34 @@ public class OrderApiController {
         return orderQueryRepository.findOrderQueryDtos();
     }
 
+    /**
+     * Query : 루트 1번, 컬렉션 1번 실행
+     * ToOne 관계들을 먼저 조회하고, 여기서 얻은 식별자 orderId로 ToMany 관계인 OrderItem 을 한꺼번에 조회
+     * MAP 을 사용해서 매칭 성능 상향
+     */
     @GetMapping("/api/v5/orders")
     public List<OrderQueryDto> ordersV5() {
         return orderQueryRepository.findAllByDto_optimization();
+    }
+
+    /**
+     * Query : 1번
+     * 단점
+     *    - 쿼리는 한번이지만 조인으로 인해 DB에서 애플리케이션에 전달하는 데이터에 중복 데이터가 추가되므로 상황에 따라 V5 보다 느릴 수 있다
+     *    - 애플리케이션에서 추가 작업이 크다
+     *    - 페이징 불가능
+     */
+    @GetMapping("/api/v6/orders")
+    public List<OrderQueryDto> orderV6() {
+        List<OrderFlatDto> flats = orderQueryRepository.findAllByDto_flat();
+
+        return flats.stream()
+                .collect(groupingBy(o -> new OrderQueryDto(o.getOrderId(), o.getName(), o.getOrderDate(), o.getOrderStatus(), o.getAddress()),
+                        mapping(o -> new OrderItemQueryDto(o.getOrderId(), o.getItemName(), o.getOrderPrice(), o.getCount()), toList())
+                ))
+                .entrySet().stream()
+                .map(e -> new OrderQueryDto(e.getKey().getOrderId(), e.getKey().getName(), e.getKey().getOrderDate(), e.getKey().getOrderStatus(), e.getKey().getAddress(), e.getValue()))
+                .collect(toList());
     }
 
 
@@ -152,7 +180,7 @@ public class OrderApiController {
             address = order.getDelivery().getAddress();
             orderItems = order.getOrderItems().stream()
                     .map(orderItem -> new OrderItemDto(orderItem))
-                    .collect(Collectors.toList());
+                    .collect(toList());
         }
 
     }
--- src/main/java/jpabook/springboot_jpa/repository/order/query/OrderQueryRepository.java
+++ src/main/java/jpabook/springboot_jpa/repository/order/query/OrderQueryRepository.java
@@ -98,4 +98,14 @@ public class OrderQueryRepository {
                 .getResultList();
     }
 
+    public List<OrderFlatDto> findAllByDto_flat() {
+        return em.createQuery(
+                "select new jpabook.springboot_jpa.repository.order.query.OrderFlatDto(o.id, m.name, o.orderDate, o.status, d.address, i.name, oi.orderPrice, oi.count)" +
+                        " from Order o" +
+                        " join o.member m" +
+                        " join o.delivery d" +
+                        " join o.orderItems oi" +
+                        " join oi.item i", OrderFlatDto.class
+        ).getResultList();
+    }
 }
