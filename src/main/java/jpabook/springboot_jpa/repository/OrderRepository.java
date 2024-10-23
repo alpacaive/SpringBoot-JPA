ad9a5535dffacbda6db00a5aa5e7be9feb797828
alpacaive <w6352800@naver.com>
2024-10-23 17:39:00 +0900

    QueryDSL

--- src/main/java/jpabook/springboot_jpa/repository/OrderRepository.java
+++ src/main/java/jpabook/springboot_jpa/repository/OrderRepository.java
@@ -1,9 +1,11 @@
 package jpabook.springboot_jpa.repository;
 
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.TypedQuery;
 import jakarta.persistence.criteria.*;
-import jpabook.springboot_jpa.domain.Member;
+import jpabook.springboot_jpa.domain.*;
 import jpabook.springboot_jpa.domain.Order;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
@@ -104,20 +106,34 @@ public class OrderRepository {
     /**
      * Querydsl 로 처리
      */
-//    public List<Order> findAll(OrderSearch orderSearch) {
-//
-//        QOrder order = QOrder.order;
-//        QMember member = QMember.member;
-//
-//        return query
-//                .select(order)
-//                .from(order)
-//                .join(order.member, member)
-//                .where(statusEq(orderSearch.getOrderStatus()),
-//                        nameLike(orderSearch.getMemberName()))
-//                .limit(1000)
-//                .fetch();
-//    }
+    public List<Order> findAll(OrderSearch orderSearch) {
+        JPAQueryFactory query = new JPAQueryFactory(em);
+        QOrder order = QOrder.order;
+        QMember member = QMember.member;
+
+        return query
+                .select(order)
+                .from(order)
+                .join(order.member, member)
+                .where(statusEq(orderSearch.getOrderStatus()), nameLike(orderSearch.getMemberName()))
+                .limit(1000)
+                .fetch();
+
+    }
+
+    private BooleanExpression nameLike(String memberName) {
+        if (!StringUtils.hasText(memberName)) {
+            return null;
+        }
+        return QMember.member.name.like(memberName);
+    }
+
+    private BooleanExpression statusEq(OrderStatus statusCond) {
+        if (statusCond == null) {
+            return null;
+        }
+        return QOrder.order.status.eq(statusCond);
+    }
 
 
     public List<Order> findAllWithMemberDelivery() {
@@ -131,21 +147,21 @@ public class OrderRepository {
     /**
      * - 패치 조인으로 SQL이 1번만 실행됨
      * - distinct 를 사용한 이유는 1대다 조인이 있으므로 데이터베이스 row가 증가한다.
-     *   그 결과 같은 order 엔티티의 조회 수도 증가하게 된다
-     *   JPA 의 distinct 는 SQL에 distinct를 추가하고, 더해서 같은 엔티티가 조회되면, 애플리케이션에서 중복을 걸러준다
-     *   이 예에서 order 가 페치 조인 때문에 중복 조회 되는 것을 막아준다
-     *  ( Hibernate 6.0부터는 distinct가 자동 적용 )
-     *
+     * 그 결과 같은 order 엔티티의 조회 수도 증가하게 된다
+     * JPA 의 distinct 는 SQL에 distinct를 추가하고, 더해서 같은 엔티티가 조회되면, 애플리케이션에서 중복을 걸러준다
+     * 이 예에서 order 가 페치 조인 때문에 중복 조회 되는 것을 막아준다
+     * ( Hibernate 6.0부터는 distinct가 자동 적용 )
+     * <p>
      * 단점
      * - 페이징 불가능
      */
     public List<Order> findAllWithItem() {
         return em.createQuery(
-                "select o from Order o" +
-                        " join fetch o.member m" +
-                        " join fetch o.delivery d" +
-                        " join fetch o.orderItems oi" +
-                        " join fetch oi.item i", Order.class)
+                        "select o from Order o" +
+                                " join fetch o.member m" +
+                                " join fetch o.delivery d" +
+                                " join fetch o.orderItems oi" +
+                                " join fetch oi.item i", Order.class)
                 .getResultList();
     }
 
@@ -153,18 +169,18 @@ public class OrderRepository {
      * 한계돌파
      * 페이징 + 컬렉션 엔티티를 함께 조회 문제는 이 방법으로 대부분 해결 가능
      * - 먼저 ToOne 관계를 모두 페치조인 한다.
-     *   ToOne 관계는 row 수를 증가시키지 않으므로 페이징 쿼리에 영향을 주지 않는다
+     * ToOne 관계는 row 수를 증가시키지 않으므로 페이징 쿼리에 영향을 주지 않는다
      * - 컬렉션은 지연 로딩으로 조회한다
      * - 지연 로딩은 성능 최적화를 위해 hibernate.default_batch_fetch_size, @BatchSize 를 적용한다
-     *    - hibernate.default_batch_fetch_size : 글로벌 성정
-     *    - @BatchSize : 개별 최적화
-     *    - 이 옵션을 사용하면 컬렉션이나 프록시 객체를 한꺼번에 설정한 size 만큼 IN 쿼리로 조회한다
+     * - hibernate.default_batch_fetch_size : 글로벌 성정
+     * - @BatchSize : 개별 최적화
+     * - 이 옵션을 사용하면 컬렉션이나 프록시 객체를 한꺼번에 설정한 size 만큼 IN 쿼리로 조회한다
      */
     public List<Order> findAllWithMemberDelivery(int offset, int limit) {
         return em.createQuery(
-                "select o from Order o" +
-                        " join fetch o.member m" +
-                        " join fetch o.delivery d", Order.class)
+                        "select o from Order o" +
+                                " join fetch o.member m" +
+                                " join fetch o.delivery d", Order.class)
                 .setFirstResult(offset)
                 .setMaxResults(limit)
                 .getResultList();
